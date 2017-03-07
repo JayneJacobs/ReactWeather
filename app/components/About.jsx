3fa35477947891558e391dce1f2753630bbb2da5
jjacob004c <jayne_jacobs@cable.comcast.com>
2017-03-07 08:44:54 -0500

    Updated Information to About  page3

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -4,12 +4,15 @@ var React = require('react');
  var About =  (props) => {
    return (
      <div>
-       <h3>About Component</h3>
+       <h3>About</h3>
        <p>Author: Jayne Jacobs</p>
-       <p>Web: jaynejacobs.com</p>
-       <p>Email: aynejacobs@jaynejacobs.com</p>
-      </div>
-    
+       <a href="http://jaynejacobs.com">jaynejacobs.com</a>
+        <p>If you have any comments or
+         suggestions, please feel free to email me here <a
+         href="mailto:jaynejacobs@jaynejacobs.com">jaynejacobs@jaynejacobs.com</a></p>
+
+     </div>
+
    )
  }
 module.exports = About;
