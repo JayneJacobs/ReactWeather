c84f63f9dcb074cf6fda8c176298ce33fcdbabb4
jjacob004c <jayne_jacobs@cable.comcast.com>
2017-03-07 08:22:33 -0500

    Updated Information to About  page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,7 +3,13 @@ var React = require('react');
 
  var About =  (props) => {
    return (
-     <h3>About Component</h3>
+     <div>
+       <h3>About Component</h3>
+       <p>Author: Jayne Jacobs</p>
+       <p>Web: jaynejacobs.com</p>
+       <p>Email: aynejacobs@jaynejacobs.com</p>
+      </div>
+    
    )
  }
 module.exports = About;
--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -19,8 +19,8 @@ var Nav = (props) => {
     <div>
       <h2>Nav Component</h2>
       <IndexLink to="/" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Get Weather</IndexLink>
-      <Link to="/about" activeClassName="active"  activeStyle={{fontWeight: 'bold'}}>About</Link>
       <Link to="/examples" activeClassName="active" activeStyle={{fontWeight: 'bold'}}>Examples</Link>
+      <Link to="/about" activeClassName="active"  activeStyle={{fontWeight: 'bold'}}>About</Link>
     </div>
   );
 }
