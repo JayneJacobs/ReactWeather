b3e18c46289bc9992539fa0c2adc0bf7cd532dbf
jjacob004c <jayne_jacobs@cable.comcast.com>
2017-03-09 08:22:01 -0500

    Added About Details

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,31 +1,38 @@
 var React = require('react');
 var {Link} = require('react-router');
- var About =  (props) => {
-   return (
-     <div>
-        <h1 className="text-center">About</h1>
-        <p>Author: Jayne Jacobs</p>
-        <ol>
-          <li>
-            <a href='http://jaynejacobs.com'>jaynejacobs.com</a>
-          </li>
-          <li>
-               <a href='mailto:jaynejacobs@jaynejacobs.com'>jaynejacobs@jaynejacobs.com</a>
-          </li>
-          <li>
-            <p>If you have any comments or
-             suggestions, please feel free to email me here
-            </p>
+var About = (props) => {
+    return (
+        <div>
+            <h1 className="text-center">About</h1>
+            <p>Author: Jayne Jacobs</p>
 
-          </li>
-          <li>
-            <p>If you have any comments or
-             suggestions, please feel free to email me here
-            </p>
-            <a href='https://github.com/JayneJacobs'>GitHub Link</a>
-          </li>
-        </ol>
-     </div>
-   )
- }
+                <li>
+                    <a href='http://jaynejacobs.com'>jaynejacobs.com</a>
+                </li>
+                <li>
+                      <a href='https://github.com/JayneJacobs'>GitHub Link</a>
+                </li>
+                <p>  </p>
+
+
+      <p>This is for the simplicity of just finding the current temperature in a city.
+        If you have any comments or suggestions, please feel free to email me here</p>
+              <li>
+                          <a href='mailto:jaynejacobs@jaynejacobs.com'>jaynejacobs@jaynejacobs.com</a>
+             </li>
+        <p>  </p>
+
+        <p>Here are some of my sources:  </p>
+
+              <li>
+                <a href='https://facebook.github.io/react'>JavaSript Framerwork</a>
+              </li>
+
+              <li>
+                  <a href='https://openweathermap.org'>Open Weather Map</a>
+              </li>
+
+    </div>
+  )
+}
 module.exports = About;
