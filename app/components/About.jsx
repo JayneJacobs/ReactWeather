b97f8876262696be1486d2273368be1bf2ddf958
jjacob004c <jayne_jacobs@cable.comcast.com>
2017-03-09 07:50:22 -0500

    Added Examples

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,16 +1,30 @@
 var React = require('react');
-
-
+var {Link} = require('react-router');
  var About =  (props) => {
    return (
      <div>
-       <h3>About</h3>
-       <p>Author: Jayne Jacobs</p>
-       <a href="http://jaynejacobs.com">jaynejacobs.com</a>
-        <p>If you have any comments or
-         suggestions, please feel free to email me here <a
-         href="mailto:jaynejacobs@jaynejacobs.com">jaynejacobs@jaynejacobs.com</a></p>
+        <h1 className="text-center">About</h1>
+        <p>Author: Jayne Jacobs</p>
+        <ol>
+          <li>
+            <a href='http://jaynejacobs.com'>jaynejacobs.com</a>
+          </li>
+          <li>
+               <a href='mailto:jaynejacobs@jaynejacobs.com'>jaynejacobs@jaynejacobs.com</a>
+          </li>
+          <li>
+            <p>If you have any comments or
+             suggestions, please feel free to email me here
+            </p>
 
+          </li>
+          <li>
+            <p>If you have any comments or
+             suggestions, please feel free to email me here
+            </p>
+            <a href='https://github.com/JayneJacobs'>GitHub Link</a>
+          </li>
+        </ol>
      </div>
    )
  }
