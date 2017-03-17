474e3a4c3b155f9f08e0efeff93e8db1b09a521a
jjacob004c <jayne_jacobs@cable.comcast.com>
2017-03-17 09:10:26 -0400

    corrected some typos

--- app/components/Weather.jsx
+++ app/components/Weather.jsx
@@ -53,7 +53,7 @@ var Weather = React.createClass({
 
     function renderMessage () {
       if (isLoading) {
-        return <h3 className="text-center">Fetching weather...</h3>;
+        return <h3 className="text-center">Fetching weather please visit jaynejacobs.com...</h3>;
       } else if (temp && location) {
         return <WeatherMessage temp={temp} location={location}/>;
       }
